df1296de8f828b4b5b4b79e2ec0c1a59a6a704d3
KiranDon <ushakiranmelasthri123@gmail.com>
2021-08-30 16:50:40 +0530

    finished till about us section

--- js/script.js
+++ js/script.js
@@ -9,5 +9,38 @@ document.addEventListener('click', function(e){
 });
 
 function toggleNavbar(){
-    document.querySelector('.navbar').classList.toggle('active')
+    document.querySelector('.navbar').classList.toggle('active');
+    document.querySelector('.navbar_toggler').classList.toggle('active');
 }
+
+//sticky nav
+window.addEventListener('scroll', function(){
+    if(this.pageYOffset > 50){
+        document.querySelector('header').classList.add('active');
+    }else{
+        document.querySelector('header').classList.remove('active');
+    }
+});
+
+// const header = document.querySelector('header');
+// const navHeight = header.getBoundingClientRect().height;
+
+// function stickyNav(entries){
+//     const [entry] = entries;
+//     console.log(entry);
+//     if(!entry.isIntersecting)
+//     {
+//         document.querySelector('nav').classList.add('sticky');
+//     }
+//     else
+//     {
+//         document.querySelector('nav').classList.remove('sticky');
+//     }
+    
+// }
+
+// const headerObserver = new IntersectionObserver(stickyNav, {
+//     root: null,
+//     threshold: 0
+// });
+// headerObserver.observe(header);
